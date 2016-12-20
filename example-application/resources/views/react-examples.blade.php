2dccf00a75b88f85fa2f996ce15a557d77f5d0a4
mass10 <mass10.github@gmail.com>
2016-12-20 23:42:51 +0900

    wip

--- example-application/resources/views/react-examples.blade.php
+++ example-application/resources/views/react-examples.blade.php
@@ -5,10 +5,14 @@
 <html>
 	<head>
 		<title>React の練習ページ</title>
+		<script src="https://unpkg.com/react@15/dist/react.min.js"></script>
+		<script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
+		<style>
+		</style>
 	</head>
 	<body>
 		<form>
-			
+				
 		</form>
 	</body>
 </html>
