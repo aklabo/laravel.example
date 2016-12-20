512884a882eba3111a15eb2c2c5278fbc8d4c37c
mass10 <mass10.github@gmail.com>
2016-12-20 23:41:18 +0900

    wip

---
+++ example-application/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Laravel の練習サイト</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form>
+			<h1>Laravel の練習サイトです</h1>
+			<ul>
+				<li><a href="floating-boxes">floating-boxes</a></li>
+				<li><a href="react-examples">react-examples</a></li>
+			</ul>
+		</form>
+	</body>
+</html>
+
---
+++ example-application/resources/views/react-examples.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>React の練習ページ</title>
+	</head>
+	<body>
+		<form>
+			
+		</form>
+	</body>
+</html>
+
