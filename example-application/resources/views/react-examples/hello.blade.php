f6d1d7c85d05ebfa76dae27d174dc761e6361642
mass10 <mass10.github@gmail.com>
2016-12-30 08:17:02 +0900

    wip

--- example-application/resources/views/react-examples/hello.blade.php
+++ example-application/resources/views/react-examples/hello.blade.php
@@ -6,20 +6,23 @@
 	<head>
 		<meta charset="utf-8">
 		<title></title>
-		<script src="https://unpkg.com/react@15/dist/react.min.js"></script>
-		<script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
-		<script>
-		function _onload() {
+		<!-- script src="https://unpkg.com/react@15/dist/react.min.js"></script -->
+		<!-- script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script -->
+		<script src="https://unpkg.com/react@latest/dist/react.js"></script>
+		<script src="https://unpkg.com/react-dom@latest/dist/react-dom.js"></script>
+		<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+	</head>
+	<body>
+		<form>
+			<div id="root">
+			</div>
+		</form>
+		<script type="text/babel">
 			ReactDOM.render(
 				<h1>Hello, world!</h1>,
 				document.getElementById('root')
 			);
-		}
 		</script>
-	</head>
-	<body onload="javascript: _onload();">
-		<form>
-		</form>
 	</body>
 </html>
 
