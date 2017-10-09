c2180904fbef2bd7ad8bf0fa259efb2435535c7c
mass10 <mass10.github@gmail.com>
2017-10-09 14:18:11 +0900

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,14 +8,6 @@
 	<body>
 		<form>
 			<h1>Laravel と HTML の練習サイトです</h1>
-			<ul>
-				<li><a href="/floating-boxes">floating-boxes</a></li>
-				<li><a href="/react-examples">react-examples</a></li>
-				<li><a href="/bootstrap-examples">bootstrap-examples</a></li>
-				<li><a href="/vue-examples">vue-examples</a></li>
-			</ul>
-
-			tes
 		</form>
 	</body>
 </html>
