a01f5ca5c7e70f051e0a90b455de894721e7b9ee
mass10 <mass10.github@gmail.com>
2017-10-09 15:22:10 +0900

    wi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 	<head>
-		<title>Laravel の練習サイト</title>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<title></title>
 	</head>
 	<body>
 		<form>
-			<h1>Laravel と HTML の練習サイトです</h1>
+			<h1></h1>
 		</form>
 	</body>
 </html>
